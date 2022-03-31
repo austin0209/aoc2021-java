ee4a5963afb598e6a1e96154f99f065e53629d88
Austin Aurelio <austin@delveam.com>
2022-03-31 00:46:25 -0500

    solve day 5 part 1

--- src/me/austin0209/aoc/Day5.java
+++ src/me/austin0209/aoc/Day5.java
@@ -1,24 +1,26 @@
 package me.austin0209.aoc;
 
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Day5 {
+    static int worldWidth = 1000;
+    static int worldHeight = 1000;
+
     record Point(int x, int y) {}
 
     record Line(Point start, Point end) {
         boolean isHorizontal() { return start.y == end.y; }
         boolean isVertical() { return start.x == end.x; }
 
-        List<Point> getPointRangeHorizontalOrVertical() {
+        List<Point> getPointRange() {
             if (isHorizontal()) {
                 int first = Math.min(start.x, end.x);
                 int last = Math.max(start.x, end.x);
@@ -59,38 +61,26 @@ public class Day5 {
         return rawLines.map(Day5::stringToLine).toList();
     }
 
-    static int getIntersectionCountHorizontalOrVertical(Line line1, Line line2) {
-        var line1Points = line1.getPointRangeHorizontalOrVertical();
-        var line2Points = line2.getPointRangeHorizontalOrVertical();
-
-        assert line1Points != null;
-        assert line2Points != null;
-
-        int count = 0;
-        for (var p1 : line1Points) {
-            for (var p2 : line2Points) {
-                if (p1.equals(p2)) count++;
-            }
-        }
-
-        return count;
-    }
-
-    static int solveDay1(List<Line> input) {
+    static long solveDay1(List<Line> input) {
         List<Line> lines = input.stream()
                 .filter(l -> l.isVertical() || l.isHorizontal())
                 .toList();
 
-        int count = 0;
-        // TODO: consider the condition where AT LEAST 2 lines must overlap.
-        // (Probably something with double counting going wrong here)
-        for (int i = 0; i < lines.size() - 1; i++) {
-            for (int j = i + 1; j < lines.size(); j++) {
-                count += getIntersectionCountHorizontalOrVertical(lines.get(i), lines.get(j));
+        List<Integer> world = new ArrayList<>(worldWidth * worldHeight);
+        while(world.size() < worldWidth * worldHeight) world.add(0);
+
+        for (var line : lines) {
+            var points = line.getPointRange();
+            for (int i = 0; i < points.size(); i++) {
+                var point = points.get(i);
+                var index = point.x + point.y * worldWidth;
+                world.set(index, world.get(index) + 1);
             }
         }
 
-        return count;
+        return world.stream()
+                .filter(i -> i >= 2)
+                .count();
     }
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -137,6 +127,7 @@ public class Day5 {
 
     @Test
     void testPart1() throws FileNotFoundException {
-        assert(solveDay1(parseInput("input/day5sample.txt")) == 5);
+        System.out.println(solveDay1(parseInput("input/day5sample.txt")));
+        assert solveDay1(parseInput("input/day5sample.txt")) == 5;
     }
 }
